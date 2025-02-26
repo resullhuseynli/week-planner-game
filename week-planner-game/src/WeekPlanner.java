e4a6f31a00526c2b694fc366dc4b2785de33f42b
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-27 00:11:17 +0400

    Merge Done!

--- week-planner-game/src/WeekPlanner.java
+++ week-planner-game/src/WeekPlanner.java
@@ -32,13 +32,24 @@ public class WeekPlanner {
     }
 
     private static String showTask(String day, String[][] weekDays) {
-
-        for (String[] weekDay : weekDays) {
-            if (weekDay[0].equalsIgnoreCase(day)) {
-                return weekDay[1];
-            }
+        switch (day.toLowerCase()) {
+            case "monday":
+                return "Your task for Monday: " + weekDays[0][1];
+            case "tuesday":
+                return "Your task for Tuesday: " + weekDays[1][1];
+            case "wednesday":
+                return "Your task for Wednesday: " + weekDays[2][1];
+            case "thursday":
+                return "Your task for Thursday: " + weekDays[3][1];
+            case "friday":
+                return "Your task for Friday: " + weekDays[4][1];
+            case "saturday":
+                return "Your task for Saturday: " + weekDays[5][1];
+            case "sunday":
+                return "Your task for Sunday: " + weekDays[6][1];
+            default:
+                return "Sorry, I don't understand you, please try again.";
         }
-        return "Sorry, I don't understand you, please try again.";
     }
 
     private static String changeTask(String day, String[][] weekDays) {
