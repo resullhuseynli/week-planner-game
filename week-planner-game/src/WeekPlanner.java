4b6340d6862fe5cb2011e7313ffbb94d32a7a201
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-26 22:25:00 +0400

    Show Task Method Created!

--- week-planner-game/src/WeekPlanner.java
+++ week-planner-game/src/WeekPlanner.java
@@ -25,11 +25,32 @@ public class WeekPlanner {
             } else if (day.startsWith("change")) {
                 System.out.println(changeTask(day, weekDays));
             } else {
-                // BURA QALIB
-//                System.out.println(showTask(day, weekDays));
+                System.out.println(showTask(day, weekDays));
             }
         }
 
+    }
+
+    private static String showTask(String day, String[][] weekDays) {
+
+        switch (day.toLowerCase()) {
+            case "monday":
+                return "Your task for Monday: " + weekDays[0][1];
+            case "tuesday":
+                return "Your task for Tuesday: " + weekDays[1][1];
+            case "wednesday":
+                return "Your task for Wednesday: " + weekDays[2][1];
+            case "thursday":
+                return "Your task for Thursday: " + weekDays[3][1];
+            case "friday":
+                return "Your task for Friday: " + weekDays[4][1];
+            case "saturday":
+                return "Your task for Saturday: " + weekDays[5][1];
+            case "sunday":
+                return "Your task for Sunday: " + weekDays[6][1];
+            default:
+                return "Sorry, I don't understand you, please try again.";
+        }
     }
 
     private static String changeTask(String day, String[][] weekDays) {
